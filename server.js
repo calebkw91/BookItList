5d8c428a0e45302ffb4fe8950cc325b15ae93593
calebkw <walke1291@gmail.com>
2021-01-16 10:43:12 -0600

    added travis.yml and fixed server.js issues

--- server.js
+++ server.js
@@ -4,15 +4,15 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -32,8 +32,8 @@ require("./routes/")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
   });
 });
