cc9ace451a7e83b6969eb2d4c35a46dd853984c4
calebkw <walke1291@gmail.com>
2021-01-16 12:08:35 -0600

    created book and user models, added db schema

---
+++ models/book.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+    let Book = sequelize.define('Book', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        pages: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        userNotes: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+
+    Book.associate = (models) => {
+        models.Book.belongsTo(models.User, {
+            onDelete: 'CASCADE',
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Book;
+};
