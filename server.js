9dbd200b36c06cd21d3fc43a6d12fea1c7a12cc6
calebkw <walke1291@gmail.com>
2021-01-16 11:14:53 -0600

    fixed travis issues

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-const express = require("express");
+const express = require('express');
 
 // Sets up the Express App
 // =============================================================
@@ -12,28 +12,28 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-const db = require("./models");
+const db = require('/models');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Handlebars engine
-const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Routes
 // =============================================================
-require("./routes/")(app);
+require('./routes/')(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
+    app.listen(PORT, () => {
+        console.log('App listening on PORT ' + PORT);
+    });
 });
