69f7a8a24be0f29fc3a2354323f35834fbdd17a4
Dj Meechlouch <Meechlouch@gmail.com>
2021-01-20 14:14:29 -0600

    added new files and changed some names to get things to connect but all working

--- models/book.js
+++ models/book.js
@@ -1,52 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-    let Book = sequelize.define('Book', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+    let Books = sequelize.define(
+        'Books',
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1],
+                },
+            },
+            author: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            genre: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            year: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            pages: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            userNotes: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            bookedIt: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+                allowNull: true,
+            },
         },
-        author: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        genre: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        year: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        pages: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        userNotes: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        bookedIt: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    });
+        { freezeTableName: true }
+    );
 
-    // Book.associate = (models) => {
-    //     models.Book.belongsTo(models.User, {
-    //         onDelete: 'CASCADE',
-    //         foreignKey: {
-    //             allowNull: false
-    //         }
-    //     });
-    // };
-
-    return Book;
+    return Books;
 };
