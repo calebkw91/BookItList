ddb113147ba89743e7d394f55f3ab8c22dfec91f
Ben Flynn <flynn0087@gmail.com>
2021-01-19 21:24:15 -0600

    server.js file updated for oauth

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,27 @@
-
-let path = require('path');
+// const path = require('path');
 
 // Routes
 module.exports = function(app) {
 
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    app.get('/cms', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/cms.html'));
-    });
-
-    app.get('/index', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    app.get('/authors', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/author-manager.html'));
-    });
+    app.get('/',
+        function(req, res) {
+            res.render('home', { user: req.user });
+        });
+    app.get('/login',
+        function(req, res){
+            res.render('login');
+        });
+    app.get('/login/google',
+        passport.authenticate('google'));
+    app.get('/return',
+        passport.authenticate('google', { failureRedirect: '/login' }),
+        function(req, res) {
+            res.redirect('/');
+        });
+    app.get('/profile',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, res){
+            res.render('profile', { user: req.user });
+        });
 
 };
--- server.js
+++ server.js
@@ -4,19 +4,75 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const Strategy = require('passport-google-oauth20').Strategy;
+// Configure the Facebook strategy for use by Passport.
+
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(
+    new Strategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/return',
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            // In this example, the user's Facebook profile is supplied as the user
+            // record.  In a production-quality application, the Facebook profile should
+            // be associated with a user record in the application's database, which
+            // allows for account linking and authentication with other identity
+            // providers.
+            return cb(null, profile);
+        }
+    )
+);
+
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    require('express-session')({
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Requiring our models for syncing
 const db = require('./models');
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // Static directory
 app.use(express.static('public'));
