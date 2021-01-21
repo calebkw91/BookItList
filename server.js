f0310927907cddffb6fd9a5d4e988ad151abf978
Ben Flynn <flynn0087@gmail.com>
2021-01-21 11:15:16 -0600

    corrections to server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@
 // =============================================================
 require('dotenv').config();
 const express = require('express');
-const passport = require('passport');
-const Strategy = require('passport-google-oauth20').Strategy;
-// Configure the Facebook strategy for use by Passport.
+let passport = require('passport');
+let Strategy = require('passport-google-oauth20').Strategy;
+// Configure the google strategy for use by Passport.
 
 // OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
+// credential (`accessToken`) for accessing the google API on the user's
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
@@ -22,13 +22,13 @@ passport.use(
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/return',
         },
-        function (accessToken, refreshToken, profile, cb) {
-            // In this example, the user's Facebook profile is supplied as the user
-            // record.  In a production-quality application, the Facebook profile should
+        function (accessToken, refreshToken, email, cb) {
+            // In this example, the user's google email is supplied as the user
+            // record.  In a production-quality application, the google profile should
             // be associated with a user record in the application's database, which
             // allows for account linking and authentication with other identity
             // providers.
-            return cb(null, profile);
+            return cb(null, email);
         }
     )
 );
@@ -38,10 +38,10 @@ passport.use(
 // production-quality application, this would typically be as simple as
 // supplying the user ID when serializing, and querying the user record by ID
 // from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
+// example does not have a database, the complete google email profile is serialized
 // and deserialized.
 passport.serializeUser(function (user, cb) {
-    cb(null, user);
+    cb(null, user.email);
 });
 
 passport.deserializeUser(function (obj, cb) {
