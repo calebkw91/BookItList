9df5ef54c71825455eb0d0e7c17585e6d98bab73
calebkw <walke1291@gmail.com>
2021-01-22 14:30:16 -0600

    changed user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
     let User = sequelize.define('User', {
-        username: {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
             validate: {
-                len: [8]
+                isEmail: true
             }
         },
         password: {
@@ -12,13 +13,6 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             validate: {
                 len: [8]
-            }
-        },
-        fullName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
             }
         }
     });
@@ -28,6 +22,16 @@ module.exports = (sequelize, DataTypes) => {
     //         onDelete: 'CASCADE'
     //     });
     // };
+
+    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    User.addHook('beforeCreate', function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
 
     return User;
 };
